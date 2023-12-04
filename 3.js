dfae20f8d17a3e9b8033fc3c92e85cb0ba2edee7
kekojeda <kekojeda@gmail.com>
2023-12-04 15:39:42 -0300

    .

---
+++ 3.js
@@ -0,0 +1,41 @@
+const original = 'abcdw'
+const modified = 'abcdww'
+
+
+function findNaughtyStep(original, modified) {
+    if (original === modified) return ''
+
+    for (let i = 0; i < original.length; i++) {
+
+      if (original[i] !== modified[i]) {
+
+        return original.length > modified.length ? original[i] : modified[i]
+
+      }
+
+    }
+
+    return modified.slice(original.length)
+  }
+
+
+
+
+
+original === modified && console.log("son iguales");
+
+
+
+for (let i = 0; i < original.length; i++) {
+    
+    
+}
+
+
+original > modified && console.log(original.split("").filter(step => !modified.includes(step)).join("")); 
+
+original < modified && console.log(modified.split("").filter(step => !original.includes(step)).join(""));
+
+
+
+
