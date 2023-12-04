ef902642e1d8e30c6d035cbd2267d640008d2aa1
Sergio Ojeda <keko@MacBook-Pro-de-Sergio.local>
2023-12-04 20:55:28 -0300

    .

--- 4.js
+++ 4.js
@@ -1,18 +1,61 @@
 const a = 'hola (odnum)'
-
-console.log(a.substring(a.indexOf("("), a.indexOf(")") ));
-
-
-console.log(a.slice(a.indexOf("("), a.indexOf(")") + 1));
+const b = 'sa(u(cla)atn)s'
 
 
-let invertir = a.slice(a.indexOf("("), a.indexOf(")") + 1)
+let indexFirstOP = b.indexOf("(")
+let indexSecondOP = b.indexOf("(", indexFirstOP+1)
 
-let result = ""
+let indexFirstCP = b.indexOf(")")
+let indexSecondCP = b.indexOf(")", indexFirstCP+1)
 
-for (let i = invertir.length -2; i > 0 ; i--) {
-    result += invertir[i];
+if(indexSecondOP != -1 ){
     
+    for (let i = b.length -1 ; i >= 0 ; i--) {
+        cadenaInvertida += cadenaAInvertir[i];
+    }
+
+
+
 }
 
-console.log(result);
+
+
+
+console.log(indexFirstOP);
+console.log(indexSecondOP);
+
+console.log(indexFirstCP);
+console.log(indexSecondCP);
+
+let bArr = b.split("")
+
+
+
+function decode(message) {
+
+    let cadenaAInvertir = message.substring(message.indexOf(("(")), message.indexOf(")") +1 )
+    // Code here
+    return cadenaAInvertir
+  }
+
+  console.log(decode(b));
+
+  ('sa(u(cla)atn)s')
+
+  saulcatns
+
+
+let cadenaAInvertir = a.slice(a.indexOf("(") + 1, a.indexOf(")"))
+
+console.log(cadenaAInvertir);
+
+let cadenaInvertida = ""
+
+for (let i = cadenaAInvertir.length -1 ; i >= 0 ; i--) {
+    cadenaInvertida += cadenaAInvertir[i];
+}
+
+let result = a.slice(0, a.indexOf("("))
+
+
+console.log(result + cadenaInvertida);
